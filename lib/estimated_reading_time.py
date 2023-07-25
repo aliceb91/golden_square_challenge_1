4eba73eac538198b9dc4a7d2b6e4e5b6033ba8fb
Claudia_Alves <claudiascarvalves@gmail.com>
2023-07-25 14:44:32 +0100

    Claudia changes

--- lib/estimated_reading_time.py
+++ lib/estimated_reading_time.py
@@ -1,3 +1,5 @@
+import datetime
+
 def estimated_reading_time(text):
     """
     Estimates the required reading time of a text message, based on a speed of 200 words per minute.
@@ -10,4 +12,11 @@ def estimated_reading_time(text):
 
     Side effects: This function doesn't print anything or have any other side efects
     """
-    return "Estimated reading time: 00:01:00.00"
+    word_second = 200/60
+    string_list = len(text.split())
+    seconds = string_list / word_second
+    time_string = str(datetime.timedelta(seconds=seconds))
+    if seconds % 1 == 0:
+        return f"Estimated reading time: 0{time_string}.00"
+    else:
+        return f"Estimated reading time: 0{time_string[:10]}"
--- tests/test_estimated_reading_time.py
+++ tests/test_estimated_reading_time.py
@@ -4,8 +4,14 @@ from lib.estimated_reading_time import estimated_reading_time
 Given a text of 200 words
 It returns a string denoting the time of 00:01:00.00
 """
-def retruns_1_minute_for_200_words():
+def test_retruns_1_minute_for_200_words():
     test_string = "Hello " * 200
     test_string = test_string[:-1]
     result = estimated_reading_time(test_string)
     assert result == "Estimated reading time: 00:01:00.00"
+
+def test_returns_15sec():
+    test_string = "Hello " * 50
+    test_string = test_string[:-1]
+    result = estimated_reading_time(test_string)
+    assert result == "Estimated reading time: 00:00:15.00"
