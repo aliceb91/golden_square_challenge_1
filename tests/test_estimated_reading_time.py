8436f18c291c36c6c3fb61d066295158ff5c5669
Alice Birkett <alice.birkett@outlook.com>
2023-07-25 14:17:29 +0100

    pre-pair push

--- tests/test_estimated_reading_time.py
+++ tests/test_estimated_reading_time.py
@@ -4,7 +4,6 @@ from lib.estimated_reading_time import estimated_reading_time
 Given a text of 200 words
 It returns a string denoting the time of 00:01:00.00
 """
-
 def retruns_1_minute_for_200_words():
     test_string = "Hello " * 200
     test_string = test_string[:-1]
